4c4886cedf59b1394253b2bdb9866462a7af32ad
Igor Rocha <igor.nathan.334@gmail.com>
2026-02-19 19:38:40 -0300

    refactor: Remove @styles alias from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,6 @@ export default defineConfig({
     alias: {
       // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
-      '@styles': path.resolve(__dirname, './src/styles'),
     },
   },
 
